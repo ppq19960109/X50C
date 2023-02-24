890e829d490c75d0cdd5294672f05dd7bf6c6a30
pangpengqiang <pangpengqiang@163.com>
2023-02-24 09:23:21 +0800

    0224

--- x50/pkg_tool/pkg.sh
+++ x50/pkg_tool/pkg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd `dirname $0`
-SOFTWARE_VERSION="2.0.0"
+SOFTWARE_VERSION="2.0.1"
 
 #compress directory and compress file name
 PKG_DIRNAME="install"
--- x50/src/cloud_platform_task.h
+++ x50/src/cloud_platform_task.h
@@ -9,7 +9,7 @@
 #define PROFILE_NAME "DevProfile.json"
 #define QUAD_NAME "DevQuad.json"
 #define ETH_NAME "wlan0"
-#define SOFTER_VER "2.0.0"
+#define SOFTER_VER "2.0.1"
 
 enum LINK_VALUE_TYPE
 {
